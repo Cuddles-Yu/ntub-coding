d0bb2212c40a9e6a78df8dec380c785587f6cc04
CuddlesYu <bob920716@gmail.com>
2024-03-17 23:22:47 +0800

    新進度 - 實作Python爬蟲V
    1.新增地點資訊欄位「留言數、留言」
    2.取得所有評論內容並以字串的方式儲存
    3.留言數為過濾掉不包含文字剩餘的評論總數
    4.新增.gitignore檔案，排除不需要加入版本控制的檔案

---
+++ 工作檔案/地圖資訊爬蟲/develop.py
@@ -0,0 +1,5 @@
+
+import pyautogui
+
+# pyautogui.hotkey('ctrl', 't', interval=0.1)
+# print(f'\r目前搜尋進度[{str(i+1).zfill(len(str(maxCount)))}/{maxCount} ({round((i+1) * 100 / maxCount, 2)}%)]', end='')
