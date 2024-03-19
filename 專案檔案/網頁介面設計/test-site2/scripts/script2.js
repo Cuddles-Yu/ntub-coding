cbac504112ca69772aed3f858b75d7b497965470
Vission843 <1020633@gmail.com>
2024-03-20 00:59:35 +0800

    [網頁介面設計]進度 - 增加網路識別碼
    1.新增網路識別碼功能
    2.增加網頁icon、排版

---
+++ 專案檔案/網頁介面設計/test-site2/scripts/script2.js
@@ -0,0 +1,9 @@
+document.getElementById('menu').addEventListener('click', function() {
+    var menu = document.getElementById('menu');
+    if (menu.style.display === 'block') {
+      menu.style.display = 'none';
+    } else {
+      menu.style.display = 'block';
+    }
+  });
+  
