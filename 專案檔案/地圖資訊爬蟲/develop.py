8cc9ba42bf55772d26744bf026bae49fa57e3870
CuddlesYu <bob920716@gmail.com>
2024-03-23 02:43:33 +0800

    [地圖資訊爬蟲]進度 - 新增相關與最新留言欄位
    1.新增地點資訊欄位「相關留言、最新留言」
    2.爬取相關留言與最新留言前10筆資料
    3.利用ActionChains套件來提升捲動頁面的效率

--- 專案檔案/地圖資訊爬蟲/develop.py
+++ 專案檔案/地圖資訊爬蟲/develop.py
@@ -3,3 +3,13 @@ import pyautogui
 
 # pyautogui.hotkey('ctrl', 't', interval=0.1)
 # print(f'\r目前搜尋進度[{str(i+1).zfill(len(str(maxCount)))}/{maxCount} ({round((i+1) * 100 / maxCount, 2)}%)]', end='')
+
+# data['留言內容'].append(f'[{", ".join(comments)}]')
+
+# 1.模擬滑鼠右鍵打開功能選單來取得座標位置
+# canvas = driver.find_element(By.XPATH, "//*[name()='canvas']")
+# ActionChains(driver).context_click(canvas).perform()
+# WebDriverWait(driver, 10).until(
+#     EC.presence_of_element_located((By.CLASS_NAME, 'mLuXec'))
+# )
+# data['座標'].append(driver.find_element(By.CLASS_NAME, 'mLuXec').text)
--- 專案檔案/地圖資訊爬蟲/main.py
+++ 專案檔案/地圖資訊爬蟲/main.py
@@ -11,22 +11,28 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.action_chains import ActionChains
 
-CommentBehavior = {
-    'Write': 0,
-    'Search': 1,
-    'Order': 2
+ButtonType = {
+    '撰寫評論': 0,
+    '查詢評論': 1,
+    '排序評論': 2
 }
 OrderType = {
-    'Related': 0,
-    'Latest': 1,
-    'TopStart': 2,
-    'BottomStart': 3
+    '最相關': 0,
+    '最新': 1,
+    '評分最高': 2,
+    '評分最低': 3
+}
+TabType = {
+    '總覽': 0,
+    '評論': 1,
+    '簡介': 2
 }
 
 # 初始化 Driver
 print('\r正在連線到GoogleMap...', end='')
 options = webdriver.EdgeOptions()
 options.add_experimental_option("detach", True)
+# options.add_argument("--headless")  # 不顯示視窗
 driver = webdriver.Edge(options=options)
 driver.get('https://www.google.com.tw/maps/preview')
 
@@ -36,7 +42,7 @@ WebDriverWait(driver, 10).until(
     EC.presence_of_element_located((By.CLASS_NAME, 'searchboxinput'))
 )
 element = driver.find_element(By.CLASS_NAME, 'searchboxinput')
-keywords = '國立臺北商業大學 附近的蛋塔'
+keywords = '臺北商業大學 附近的蛋塔'
 element.send_keys(keywords)
 element.send_keys(Keys.ENTER)
 
@@ -46,17 +52,19 @@ WebDriverWait(driver, 10).until(
     EC.presence_of_element_located((By.CLASS_NAME, 'Nv2PK'))
 )
 resultContainer = driver.find_element(By.XPATH, '/html/body/div[1]/div[3]/div[8]/div[9]/div/div/div[1]/div[2]/div/div[1]/div/div/div[1]/div[1]')
-# while True:
-#     try:
-#         driver.find_element(By.CLASS_NAME, 'HlvSq')
-#         break
-#     except NoSuchElementException:
-#         resultContainer.send_keys(Keys.PAGE_DOWN)
+while True:
+    try:
+        driver.find_element(By.CLASS_NAME, 'HlvSq')
+        break
+    except NoSuchElementException:
+        ActionChains(driver).move_to_element(resultContainer.find_elements(By.CLASS_NAME, 'Nv2PK')[-1]).perform()
+        resultContainer.send_keys(Keys.PAGE_DOWN)
+        time.sleep(0.1)
 
 print('\r正在建立資料...', end='')
-titles = driver.find_elements(By.CLASS_NAME, 'hfpxzc')
-values = driver.find_elements(By.CLASS_NAME, 'MW4etd')
-comments = driver.find_elements(By.CLASS_NAME, 'UY7F9')
+titles = driver.find_elements(By.CLASS_NAME, 'hfpxzc')  # 地點名稱
+values = driver.find_elements(By.CLASS_NAME, 'MW4etd')  # 平均評分
+comments = driver.find_elements(By.CLASS_NAME, 'UY7F9')  # 評分總數
 url = [title.get_attribute('href') for title in titles]
 data = {
     '地點名稱': [title.get_attribute('aria-label') for title in titles],
@@ -67,7 +75,8 @@ data = {
     '平均評分': [str(value.text) for value in values],
     '評分總數': [re.sub(r'\D', '', comment.text) for comment in comments],
     '留言總數': [],
-    '留言內容': []
+    '相關留言': [],
+    '最新留言': []
 }
 
 maxCount = len(url)
@@ -98,49 +107,80 @@ for i in range(maxCount):
                         data[name].append(tag.find_element(By.CLASS_NAME, 'Io6YTe').text)
     data['地圖標籤'].append(driver.find_element(By.CLASS_NAME, 'DkEaL').text)
 
+    # 變數宣告'評分總數'
+    totalCommentCount = int(data['評分總數'][i])
+    # 標籤按鈕 - 總覽/[評論]/簡介
     WebDriverWait(driver, 10).until(
         EC.presence_of_element_located((By.CLASS_NAME, 'RWPxGd'))
     )
-    commentTab = driver.find_element(By.CLASS_NAME, 'RWPxGd').find_elements(By.CLASS_NAME, 'hh2c6')[1]
-    commentTab.click()
-
+    tabs = driver.find_element(By.CLASS_NAME, 'RWPxGd').find_elements(By.CLASS_NAME, 'hh2c6')
+    tabs[TabType['評論']].click()
+    # 評論面板
     commentContainer = driver.find_element(By.CLASS_NAME, 'dS8AEf')
 
+    # --- 【取得最相關與最新留言前10筆】 ---
+    storeCount = 10
+    # 功能按鈕 - 撰寫/查詢/[排序評論]
     WebDriverWait(driver, 10).until(
         EC.presence_of_element_located((By.CLASS_NAME, 'S9kvJb'))
     )
     orderButton = driver.find_elements(By.CLASS_NAME, 'S9kvJb')
-    orderButton[CommentBehavior['Order']].click()
+    orderButton[ButtonType['排序評論']].click()
+    # 排序選單 - 最相關/[最新]/評分最高/評分最低
     WebDriverWait(driver, 10).until(
         EC.presence_of_element_located((By.CLASS_NAME, 'fxNQSd'))
     )
     orderList = driver.find_elements(By.CLASS_NAME, 'fxNQSd')
-    orderList[OrderType['Latest']].click()
-
-    while True:
-        commentContainer.send_keys(Keys.PAGE_DOWN)
-        if len(commentContainer.find_elements(By.CLASS_NAME, 'jftiEf')) >= int(data['評分總數'][i]):
-            break
-    allComments = commentContainer.find_elements(By.CLASS_NAME, 'jftiEf')
-    comments = []
-    data['留言總數'].append(str(len(commentContainer.find_elements(By.CLASS_NAME, 'Upo0Ec'))))
-    for c in range(len(allComments)):
-        print(f'\r正在取得所有評論(%d/%d)...' % (c + 1, len(allComments)), end='')
+    orderList[OrderType['最新']].click()
+    WebDriverWait(driver, 10).until(
+        EC.presence_of_element_located((By.CLASS_NAME, 'jftiEf'))
+    )
+    comments = commentContainer.find_elements(By.CLASS_NAME, 'jftiEf')
+    latestComments = []
+    for comment in comments[0:storeCount] if len(comments) >= storeCount else comments:
         try:
-            comments.append(allComments[c].find_element(By.CLASS_NAME, 'wiI7pd').text)
+            latestComments.append(comment.find_element(By.CLASS_NAME, 'wiI7pd').text)
         except:
             pass
-    data['留言內容'].append(f'[{", ".join(comments)}]')
+    data['最新留言'].append(', '.join(latestComments))
+    orderButton[ButtonType['排序評論']].click()
+    # 排序選單 - [最相關]/最新/評分最高/評分最低
+    WebDriverWait(driver, 10).until(
+        EC.presence_of_element_located((By.CLASS_NAME, 'fxNQSd'))
+    )
+    orderList = driver.find_elements(By.CLASS_NAME, 'fxNQSd')
+    orderList[OrderType['最相關']].click()
+    WebDriverWait(driver, 10).until(
+        EC.presence_of_element_located((By.CLASS_NAME, 'jftiEf'))
+    )
+    comments = commentContainer.find_elements(By.CLASS_NAME, 'jftiEf')
+    relatedComments = []
+    for comment in comments[0:storeCount] if len(comments) >= storeCount else comments:
+        try:
+            relatedComments.append(comment.find_element(By.CLASS_NAME, 'wiI7pd').text)
+        except:
+            pass
+    data['相關留言'].append(', '.join(relatedComments))
+    # 滾動評論面板取得所有評論
+    while True:
+        ActionChains(driver).move_to_element(commentContainer.find_elements(By.CLASS_NAME, 'jftiEf')[-1]).perform()
+        commentContainer.send_keys(Keys.PAGE_DOWN)
+        currentCount = len(commentContainer.find_elements(By.CLASS_NAME, 'jftiEf'))
+        print(f'\r正在取得所有評論(%d/%d)...' % (currentCount, totalCommentCount), end='')
+        if currentCount >= totalCommentCount:
+            break
+        time.sleep(0.1)
+    data['留言總數'].append(str(len(commentContainer.find_elements(By.CLASS_NAME, 'Upo0Ec'))))
 
-    # 1.模擬滑鼠右鍵打開功能選單來取得座標位置
-    # canvas = driver.find_element(By.XPATH, "//*[name()='canvas']")
-    # ActionChains(driver).context_click(canvas).perform()
-    # WebDriverWait(driver, 10).until(
-    #     EC.presence_of_element_located((By.CLASS_NAME, 'mLuXec'))
-    # )
-    # data['座標'].append(driver.find_element(By.CLASS_NAME, 'mLuXec').text)
+    # 取得所有留言並重新計算評分
+    # allComments = commentContainer.find_elements(By.CLASS_NAME, 'jftiEf')
+    # for c in range(len(allComments)):
+    #     try:
+    #         # 新增留言分析以及重新計算平均評分
+    #     except:
+    #         pass
 
-    # 2.等待網址列顯示座標位置後取得座標位置
+    # 等待網址列顯示座標位置後取得座標位置
     print('\r正在取得地點座標...', end='')
     if len(data['地址']) == i+1:
         while True:
@@ -159,6 +199,7 @@ for i in range(maxCount):
     print(f'\r進度 {str(i + 1).zfill(len(str(maxCount)))}/{maxCount}, {progressPercentage}% [{"▮" * int(progressPercentage // 10 + 1)}{"▯" * int(10 - (progressPercentage // 10 + 1))}]', end='')
 
 print('\r正在輸出csv搜尋結果...', end='')
+# print(data)
 frame = pd.DataFrame(data)
 frame.to_csv(format('%s的搜尋結果.csv' % keywords.strip().replace(' ','')), encoding='utf-8-sig')
 print('\rcsv搜尋結果已完成輸出！', end='')
