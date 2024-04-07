8bb6126345ec6f8bc673bff7d83638032a825023
Vission843 <1020633@gmail.com>
2024-04-08 00:38:29 +0800

    [網頁介面設計]進度 - 新增展開框滾動-2
    調整與展開框相關設定

--- 專案檔案/網頁介面設計/test-site2/scripts/script2.js
+++ 專案檔案/網頁介面設計/test-site2/scripts/script2.js
@@ -1,9 +1,20 @@
-document.getElementById('menu').addEventListener('click', function() {
-    var menu = document.getElementById('menu');
-    if (menu.style.display === 'block') {
-      menu.style.display = 'none';
-    } else {
-      menu.style.display = 'block';
-    }
+var accordionItems = document.querySelectorAll('.accordion-item');
+  accordionItems.forEach(function(item) {
+    item.addEventListener('click', function() {
+      // 切換最上層項目的類別以應用不同的CSS樣式
+      if (this.classList.contains('active')) {
+        this.classList.remove('active');
+        this.querySelector('.accordion-item-content').style.maxHeight = '30em';
+      } else {
+        // 移除其他已展開的項目的類別和樣式
+        accordionItems.forEach(function(el) {
+          el.classList.remove('active');
+          el.querySelector('.accordion-item-content').style.maxHeight = null;
+        });
+        // 展開被點擊的項目
+        this.classList.add('active');
+        this.querySelector('.accordion-item-content').style.maxHeight = '15.4em';
+      }
+    });
   });
   
