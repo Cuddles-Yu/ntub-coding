636f31fda470a2ac32782d2649f9293952ac13f0
70chiu <chiuirene0329@gmail.com>
2024-05-17 01:39:09 +0800

    [地圖結果串接]進度 - 成功限制顯示字數

--- 專案檔案/地圖結果串接/comment.php
+++ 專案檔案/地圖結果串接/comment.php
@@ -1,11 +1,13 @@
 <?php
   include 'DB.php';
-  $sql = "SELECT * FROM `comments` WHERE `store_name` = '85度C 中和圓通店' AND `sort` = 1"; //指定SQL查詢字串
+  $sql = "SELECT * FROM `comments` WHERE `store_name` = '85度C 中和圓通店' AND `sort` = 1"; //特定店家的第幾筆留言
 
   //送出查詢的SQL指令
   if ($result = mysqli_query($link, $sql)) {
      while ($row = mysqli_fetch_assoc($result)) {
-        echo $row["contents"] . "<br/>";
+         $content = $row["contents"];
+         $limited_content =(mb_strlen($content)>140)? mb_substr($content,0,140).'...':$content;
+         echo $limited_content . "<br/>";
      }
      mysqli_free_result($result); //釋放占用記憶體
   } else {
