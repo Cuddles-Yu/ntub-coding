8f6bf33cf200a9c2e7e28e6b9aef3da803877713
CuddlesYu <bob920716@gmail.com>
2024-07-26 04:42:53 +0800

    [地圖資訊爬蟲]進度 - 完善查詢附近餐廳功能

--- 專案檔案/地圖資訊爬蟲/crawler/module/functions.py
+++ 專案檔案/地圖資訊爬蟲/crawler/module/functions.py
@@ -1,16 +1,17 @@
-import time
-
-import selenium.common.exceptions
-import random
-
-from 地圖資訊爬蟲.crawler.module.const import *
-
 import re
 import sys
-from 地圖資訊爬蟲.crawler.module.return_code import *
+import time
+import random
 from numpy import sin, cos, arccos, pi, round
 
+from 地圖資訊爬蟲.crawler.module.const import *
+from 地圖資訊爬蟲.crawler.tables import Store
+from 地圖資訊爬蟲.crawler.tables.base import *
+from 地圖資訊爬蟲.crawler.module.return_code import *
+
 from selenium import webdriver
+from selenium.webdriver.common.action_chains import ActionChains
+from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 from selenium.common.exceptions import TimeoutException
 from selenium.webdriver.support.ui import WebDriverWait
@@ -27,6 +28,66 @@ def init_driver(url: str):
     if url: driver.get(url)
     driver.set_window_position(x=970, y=10)
     return driver
+
+def search(driver, keyword):
+    # 等待 Driver 瀏覽到指定頁面後，對搜尋框輸入關鍵字搜尋
+    search_box = wait_for_element(By.CLASS_NAME, driver, 'searchboxinput')
+    search_box.clear()
+    search_box.send_keys(keyword)
+    search_box.send_keys(Keys.ENTER)
+
+def search_and_scroll(connection, driver, keyword):
+    while True:
+        search(driver, keyword)
+
+        # 取得所有搜尋結果所在的'容器'物件
+        print('\r正在取得搜尋結果...(可能會花費較多時間)', end='')
+        WebDriverWait(driver, MAXIMUM_TIMEOUT).until(
+            ec.presence_of_element_located((By.CLASS_NAME, 'Nv2PK'))
+        )
+        container_search_result = find_element_list(By.XPATH, driver, [
+            '/html/body/div[1]/div[3]/div[8]/div[9]/div/div/div[1]/div[2]/div/div[1]/div/div/div[1]/div[1]',
+            '/html/body/div[2]/div[3]/div[8]/div[9]/div/div/div[1]/div[2]/div/div[1]/div/div/div[1]/div[1]'
+        ])
+
+        # 紀錄爬取評論的等待時間
+        start_time = time.time()
+        current_results = 0
+        finish_scroll = True
+
+        # 向下捲動瀏覽所有搜尋結果
+        if ENABLE_SCROLL_DOWN:
+            while True:
+                if len(driver.find_elements(By.CLASS_NAME, 'HlvSq')) > 0:
+                    break
+                ActionChains(driver).move_to_element(container_search_result.find_elements(By.CLASS_NAME, 'Nv2PK')[-1]).perform()
+                container_search_result.send_keys(Keys.PAGE_DOWN)
+                time.sleep(0.1)
+                # 檢查是否持續一段時間皆未出現新的結果(卡住)
+                results = driver.find_elements(By.CLASS_NAME, 'hfpxzc')
+                if current_results != len(results): start_time = time.time()
+                current_results = len(results)
+                if time.time() - start_time > MAXIMUM_TIMEOUT:
+                    finish_scroll = not HAVE_TO_GET_ALL_RESULTS
+                    break
+        if finish_scroll: break
+
+    element_search_title = driver.find_elements(By.CLASS_NAME, 'hfpxzc')
+    # 商家連結與名稱
+    urls = to_map_url([title.get_attribute('href') for title in element_search_title])
+    store_names = [title.get_attribute('aria-label') for title in element_search_title]
+
+    # 儲存本次查詢瀏覽連結
+    print(f'\r正在建立搜尋結果至資料庫...')
+    for url, store_name in zip(urls, store_names):
+        store = Store.newObject(store_name, url)
+        if store.exists(connection):
+            print(f'✴️已存在搜尋結果【{store_name}】')
+        else:
+            store.insert_if_not_exists(connection)
+            print(f'✳️已建立搜尋結果【{store_name}】')
+
+    return urls
 
 ### 函式 ###
 def crawler_exit(driver, connection):
@@ -87,6 +148,11 @@ def exclude_list(array, c) -> list:
 def combine(str_array: list, separator: str) -> str:
     return separator.join(str_array)
 
+def click_element(driver, element):
+    if element is None: return
+    ActionChains(driver).move_to_element(element).perform()
+    element.click()
+
 def wait_for_click(by, driver, value):
     try:
         WebDriverWait(driver, MAXIMUM_WAITING).until(
@@ -109,9 +175,21 @@ def wait_for_click_index(by, driver, value, index):
     except Exception as e:
         return None
 
+def find_element_list(by, driver, values):
+    for value in values:
+        elements = driver.find_elements(by, value)
+        if elements: return elements[0]
+    return None
+
+def wait_for_element_list(by, driver, values):
+    for value in values:
+        element = wait_for_element(by, driver, value)
+        if element: return element
+    return None
+
 def wait_for_element(by, driver, value):
     try:
-        element = WebDriverWait(driver, MAXIMUM_WAITING).until(
+        WebDriverWait(driver, MAXIMUM_WAITING).until(
             ec.presence_of_element_located((by, value))
         )
         return driver.find_element(by, value)
--- 專案檔案/地圖資訊爬蟲/crawler/store_branches.py
+++ 專案檔案/地圖資訊爬蟲/crawler/store_branches.py
@@ -18,51 +18,6 @@ from selenium.webdriver.common.action_chains import ActionChains
 from selenium.webdriver.support import expected_conditions as ec
 from selenium.webdriver.support.ui import WebDriverWait
 
-def search(_driver, _url):
-    while True:
-        # 瀏覽器載入指定的連結
-        _driver.get(_url)
-
-        # 等待 Driver 瀏覽到指定頁面後，對搜尋框輸入關鍵字搜尋
-        print(f'正在搜尋關鍵字 -> {SEARCH_KEYWORD}\n')
-        search_box = wait_for_element(By.CLASS_NAME, driver, 'searchboxinput')
-        search_box.clear()
-        search_box.send_keys(SEARCH_KEYWORD)
-        search_box.send_keys(Keys.ENTER)
-
-        # 取得所有搜尋結果所在的'容器'物件
-        print('\r正在取得搜尋結果...(可能會花費較多時間)', end='')
-        WebDriverWait(driver, MAXIMUM_TIMEOUT).until(
-            ec.presence_of_element_located((By.CLASS_NAME, 'Nv2PK'))
-        )
-        container_search_result = driver.find_element(By.XPATH, '/html/body/div[2]/div[3]/div[8]/div[9]/div/div/div[1]/div[2]/div/div[1]/div/div/div[1]/div[1]')
-
-        # 紀錄爬取評論的等待時間
-        start_time = time.time()
-        current_results = 0
-        finish_scroll = True
-
-        # 向下捲動瀏覽所有搜尋結果
-        if ENABLE_SCROLL_DOWN:
-            while True:
-                if len(driver.find_elements(By.CLASS_NAME, 'HlvSq')) > 0: break
-                ActionChains(driver).move_to_element(container_search_result.find_elements(By.CLASS_NAME, 'Nv2PK')[-1]).perform()
-                container_search_result.send_keys(Keys.PAGE_DOWN)
-                time.sleep(0.1)
-                # 檢查是否持續一段時間皆未出現新的結果(卡住)
-                results = driver.find_elements(By.CLASS_NAME, 'hfpxzc')
-                if current_results != len(results): start_time = time.time()
-                current_results = len(results)
-                if time.time() - start_time > MAXIMUM_TIMEOUT:
-                    finish_scroll = not HAVE_TO_GET_ALL_RESULTS
-                    break
-
-        if finish_scroll: break
-
-    # 地圖連結
-    return [title.get_attribute('href') for title in driver.find_elements(By.CLASS_NAME, 'hfpxzc')]
-
-
 ### 主程式 ###
 if __name__ == "__main__":
     # 連線資料庫
@@ -77,13 +32,14 @@ if __name__ == "__main__":
     print(f'正在執行擴增商家 -> {NAME}\n')
     START_TIME = datetime.now()
     for i in range(len(old_urls)):
-        print(f'\r正在取得商家週邊的其他商家({i+1}/{len(old_urls)})...', end='')
-        print(search(driver, old_urls[i]))
+        print(f'\r正在取得商家週邊的其他商家({i+1}/{len(old_urls)})...\n', end='')
+        driver.get(old_urls[i])
+        search_and_scroll(connection, driver, '餐廳')
 
     # 計算時間差
     TIME_DIFFERENCE = datetime.now() - START_TIME
     MINUTES_ELAPSE = TIME_DIFFERENCE.total_seconds() / 60
 
-    print(f'\r【✅已完成】耗時:{MINUTES_ELAPSE:.2f}分鐘 | {len(result)} 組推薦餐點關鍵字', end='')
+    print(f'\r【✅已完成】耗時:{MINUTES_ELAPSE:.2f}分鐘', end='')
 
     crawler_exit(driver, connection)
